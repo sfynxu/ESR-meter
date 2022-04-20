7724fdabcea187d7ab1968a3e0d30b9828cef98f
sfynxu <badea_romeo@yahoo.com>
2022-04-20 20:12:10 +0300

    Initial setup

---
+++ FW/board_config.h
@@ -0,0 +1,4 @@
+#ifndef __BOARD_CONFIG_H__
+    #define __BOARD_CONFIG_H__
+
+#endif
---
+++ FW/main.c
@@ -0,0 +1,8 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "board_config.h"
+
+int main(void){
+    while(1){}
+    return(0);
+}
