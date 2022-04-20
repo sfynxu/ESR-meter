d207fb475ca64293dd4fabca1d76035ea386feaa
sfynxu <badea_romeo@yahoo.com>
2022-04-20 22:06:25 +0300

    Board setup

--- FW/board_config.h
+++ FW/board_config.h
@@ -1,4 +1,9 @@
 #ifndef __BOARD_CONFIG_H__
     #define __BOARD_CONFIG_H__
 
+    #define DISP_DATA   PA0
+    #define DISP_CLK    PA1
+    #define DISP_RST    PA2
+    #define DISP_STRB   PA3
+
 #endif
